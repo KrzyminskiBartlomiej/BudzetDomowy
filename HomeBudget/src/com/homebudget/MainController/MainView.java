466502d7329903025e65a9c11fb3454cd6711340
Krzyminski.Bartlomiej <krzyminski.bartlomiej@gmail.com>
2017-06-08 21:24:22 +0200

    Added JavaDoc to MainView.java

--- HomeBudget/src/com/homebudget/MainController/MainView.java
+++ HomeBudget/src/com/homebudget/MainController/MainView.java
@@ -3,11 +3,27 @@ package com.homebudget.MainController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * Application class from which JavaFX applications extend. The entry point for
+ * JavaFX applications is the Application class. The JavaFX runtime does the
+ * following, in order, whenever an application is launched.
+ * 
+ * @author Bartlomiej Krzyminski
+ * @since v1.00
+ * 
+ */
+
 public class MainView extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	/**
+	 * Application launches here. This is the place to configure and invoke each
+	 * window used by application.
+	 * 
+	 */
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
