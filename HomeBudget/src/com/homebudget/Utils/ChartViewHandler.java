6bdb42e5cc6fb892a4b45e66ed8a9d3e425ee87f
Krzyminski.Bartlomiej <krzyminski.bartlomiej@gmail.com>
2017-07-03 16:48:53 +0200

    Created dynamically series addition into PieChart, added validation which includes only non zero values into chart

--- HomeBudget/src/com/homebudget/Utils/ChartViewHandler.java
+++ HomeBudget/src/com/homebudget/Utils/ChartViewHandler.java
@@ -13,30 +13,30 @@ public class ChartViewHandler {
 	public static ObservableList<PieChart.Data> pieChartData;
 
 	public void createDataForPieChart() {
-		pieChartData = FXCollections.observableArrayList(new PieChart.Data(ConfigureView.costTypeItems.get(0), getSumForType(ConfigureView.costTypeItems.get(0))),
-				new PieChart.Data(ConfigureView.costTypeItems.get(1), getSumForType(ConfigureView.costTypeItems.get(1))),
-				new PieChart.Data(ConfigureView.costTypeItems.get(2), getSumForType(ConfigureView.costTypeItems.get(2))),
-				new PieChart.Data(ConfigureView.costTypeItems.get(3), getSumForType(ConfigureView.costTypeItems.get(3))),
-				new PieChart.Data(ConfigureView.costTypeItems.get(4), getSumForType(ConfigureView.costTypeItems.get(4))),
-				new PieChart.Data(ConfigureView.costTypeItems.get(5), getSumForType(ConfigureView.costTypeItems.get(5))),
-				new PieChart.Data(ConfigureView.costTypeItems.get(6), getSumForType(ConfigureView.costTypeItems.get(6))));
+		pieChartData = FXCollections.observableArrayList();
+		for (int i = 0; i < ConfigureView.costTypeItems.size(); i++) {
+			if (getSumForType(ConfigureView.costTypeItems.get(i)) != 0) {
+				pieChartData.add(new PieChart.Data(ConfigureView.costTypeItems.get(i),
+						getSumForType(ConfigureView.costTypeItems.get(i))));
+			}
+		}
 
 		pieChartData.forEach(
 				data -> data.nameProperty().bind(Bindings.concat(data.getName(), " ", data.pieValueProperty(), " zl")));
 	}
-	
-	public String dataSplitter(List<?> toSplit, int place){
+
+	public String dataSplitter(List<?> toSplit, int place) {
 		String sumData = (String) toSplit.get(place);
 		return sumData;
 	}
 
 	public Double getSumForType(String type) {
-		Double sumResult = 0.00;	
-		for(int i = 0; i < TableViewHandler.data.size(); i++){
-			if(type.equals(dataSplitter(TableViewHandler.data.get(i),1))){
-				sumResult += Double.parseDouble(dataSplitter(TableViewHandler.data.get(i),3));
+		Double sumResult = 0.00;
+		for (int i = 0; i < TableViewHandler.data.size(); i++) {
+			if (type.equals(dataSplitter(TableViewHandler.data.get(i), 1))) {
+				sumResult += Double.parseDouble(dataSplitter(TableViewHandler.data.get(i), 3));
 			}
-		}		
+		}
 		return sumResult;
 	}
 }
